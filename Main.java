2cc557556a14ad0a64049cd03ab0622655e7c9ec
Hiroki Kato <hirokikato1@gmail.com>
2024-06-11 16:10:19 -0400

    project scaffolding and prompt user for input

---
+++ Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome to Stra-tee-ger-eez miniHackathon submission!");
+        System.out.println("Please enter a Sales Representative's last name to retrieve the associated data: ");
+        String lastName = scanner.nextLine().trim();
+
+    }
+}
+
