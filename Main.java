65d73d52448f940067a1d14daa14bd0ce9777a86
Hiroki Kato <hirokikato1@gmail.com>
2024-06-11 16:56:01 -0400

    create method readSalesReps(filePath, repLastName) that searches lastname input and returns List of salesReps--in case there are duplicate shared last names

--- Main.java
+++ Main.java
@@ -1,3 +1,5 @@
+import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
+
 import java.util.Scanner;
 import java.io.BufferedReader;
 import java.io.FileReader;
@@ -16,24 +18,38 @@ public class Main {
 
         //Step 2: Read data from SalesReps.csv to find userID associated with sales rep
         try{
-            List<SalesRep> salesReps = readSalesReps("./src/SalesReps.csv");
+            List<SalesRep> salesReps = readSalesReps("./src/SalesReps.csv", lastName);
         }catch(Exception e){
             System.out.println(e.getMessage());
         }
-        // Method to read SalesRep data from a CSV file
-
-
     }
-    private static List<SalesRep> readSalesReps(String filePath) throws IOException {
+
+    // Method to read SalesRep data from a CSV file
+    private static List<SalesRep> readSalesReps(String filePath, String lastName) throws IOException {
         List<SalesRep> salesReps = new ArrayList<>();
-        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+        BufferedReader br = null;
+        try {
+            br = new BufferedReader(new FileReader(filePath));
             String line;
-            br.readLine(); // Skip header
+            br.readLine(); // Skip header that contains fields of csv file
             while ((line = br.readLine()) != null) {
                 String[] parts = line.split(",");
-                if (parts.length >= 3) {
-                    System.out.println(parts[0].trim() +" " + parts[1].trim() +" " + parts[2].trim());
-                    //salesReps.add(new SalesRep(parts[0].trim(), parts[1].trim(), parts[2].trim()));
+                System.out.println("comparing " + lastName + " to " + parts[2].trim());
+                System.out.println("Do they match? " + lastName.compareTo(parts[2].trim()));
+                if(lastName.compareTo(parts[2].trim()) == 0){
+                    System.out.println("we have a match! " + parts[0].trim() +" " + parts[1].trim() +" " + parts[2].trim());
+                    salesReps.add(new SalesRep(parts[0].trim(), parts[1].trim(), parts[2].trim()));
+                }
+            }
+            System.out.println("Sorry we do not have data on that Sales Rep!");
+        }catch(IOException e){
+            e.printStackTrace();
+        }finally{
+            if (br!=null){
+                try{
+                    br.close();
+                }catch(IOException e){
+                    e.printStackTrace();
                 }
             }
         }
